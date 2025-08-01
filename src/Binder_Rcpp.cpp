d38950a5cae59a987b1d3ef4eab8fe7c0291e4a0
guanyu-chen-gy <Guanyu.C@outlook.com>
2025-08-01 14:37:50 +0100

    Binder_Rcpp function (untested)

---
+++ src/Binder_Rcpp.cpp
@@ -0,0 +1,76 @@
+#include <Rcpp.h>
+#include <cmath>
+using namespace Rcpp;
+
+//' Compute "n-invariant version” of Binder distance between two partitions
+//'
+//' @param c1 An integer vector of cluster labels for $n$ items. Should be 0-indexed.
+//' @param c2 An integer vector of cluster labels for $n$ items. Should be 0-indexed.
+//' @param K1 An integer, specifying the number of unique clusters in \code{cl1}.
+//' @param K2 An integer, specifying the number of unique clusters in \code{cl2}.
+//' @return The VI distance between \code{cl1} and \code{cl2}.
+// [[Rcpp::export]]
+double Binder_compute_Rcpp(NumericVector c1, NumericVector c2, int K1, int K2){
+  int n=c1.length();
+  
+  NumericMatrix ctab(K1,K2);
+  NumericVector r(K1);
+  NumericVector c(K2);
+    for(int i = 0; i < n; i++){
+    ctab(c1[i],c2[i])++;
+  }
+  for(int k1 = 0; k1 < K1; k1++){
+    for(int k2 = 0; k2 < K2; k2++){
+      r[k1] += ctab(k1,k2);
+      c[k2] += ctab(k1,k2);
+    }
+  }
+  
+  double f = 0.0;
+  for(int k1 = 0; k1 < K1; k1++){
+    if(r[k1] > 0){
+      f += pow((r[k1]/n),2);
+    }
+  }
+  for(int k2 = 0; k2 < K2; k2++){
+    if(c[k2]>0){
+      f += pow((c[k2]/n),2);
+    }
+  }
+  for(int k1 = 0; k1 < K1; k1++){
+    for(int k2 = 0; k2 < K2; k2++){
+      if(ctab(k1,k2)>0){
+        f += -2*pow((ctab(k1,k2)/n),2);
+      }
+    }
+  }
+  return(f);
+}
+
+//' Binder distance between two groups of partitions
+//'
+//' @param cls1 A matrix of $n$ columns, where each row contains the 0-indexed cluster labels for n items. 
+//' @param cls2 A matrix of $n$ columns, where each row contains the 0-indexed cluster labels for n items. 
+//' @param K1s An integer vector, specifying the number of clusters in each row of \code{cls1}. Must have length equal to the number of rows in \code{cls1}.
+//' @param K2s An integer vector, specifying the number of clusters in each row of \code{cls2}. Must have length equal to the number of rows in \code{cls2}.
+//' @return A matrix containing the VI distance between each row of \code{cls1} and \code{cls2}.
+// [[Rcpp::export]]
+NumericMatrix Binder_Rcpp(NumericMatrix cls1,NumericMatrix cls2,NumericVector K1s,NumericVector K2s) {
+
+  if(cls1.ncol() != cls2.ncol()){
+    stop("number of data points must be the same");
+  }
+  
+  int M1=cls1.nrow();
+  int M2=cls2.nrow();
+    
+  NumericMatrix output(M1,M2);
+  
+  for (int i = 0; i < M1; i++) {
+    for(int j = 0; j < M2; j++){
+      output(i,j) = Binder_compute_Rcpp(cls1(i,_),cls2(j,_),K1s[i],K2s[j]);
+    }
+  }
+  
+  return output;
+}
