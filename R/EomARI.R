f2faed363ec3b09ac085aa7d164a639170052fa2
guanyu-chen-gy <Guanyu.C@outlook.com>
2025-08-05 18:45:58 +0100

    expected omARI

---
+++ R/EomARI.R
@@ -0,0 +1,14 @@
+#' Compute the posterior expected omARI loss for a partition
+#'
+#' @param cls A vector containing the cluster assignments of $n$ data points, representing the partition with respect to which the EVI is computed.
+#' @param cls.draw A matrix of the MCMC samples of partitions of $n$ data points, of dimensions $S × n$, where $S$ is the number of MCMC samples.
+#' @param Ks The number of clusters in \code{cls}
+#' @param Ks.draw A vector of $S$, containing the number of clusters for each of the MCMC samples.
+#' @return The posterior expected Binder for the partition \code{cls}, where the posterior is approximated by the MCMC samples \code{cls.draw}. It corresponds to the average Binder distance between \code{cls} and each MCMC sample in \code{cls.draw}.
+EomARI_Rcpp <- function(cls, cls.draw, Ks, Ks.draw) {
+  if (is.vector(cls)) cls <- t(cls)
+  S <- dim(cls.draw)[1]
+  
+  output <- sum(omARI_Rcpp(cls, cls.draw, Ks, Ks.draw)) / S
+  return(output)
+}
